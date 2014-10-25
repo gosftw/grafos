ffafee443535f23b81ef4311a1f3d99a368beee8
HP CORE i3 <HP CORE i3@192.168.0.105>
2014-10-25 14:05:07 -0400

    carga de archivos

--- src/grafosproyecto/RecorridoHamilton.java
+++ src/grafosproyecto/RecorridoHamilton.java
@@ -27,12 +27,44 @@ public class RecorridoHamilton {
             Cuarto cuAdy = cu.cuarto.get(i);
             String cadena = new String();
             cadena = cadena.concat(cu.dato);
-            hamiltonRecursivo(cu,cuAdy);
+            cu.visitado=true;
+            hamiltonRecursivo(cadena, cu,cuAdy);
         }
     }
 
-    private void hamiltonRecursivo(Cuarto cu, Cuarto cuAdy) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void hamiltonRecursivo(String cadena, Cuarto cu, Cuarto cuAdy) {
+        cadena=cadena.concat(cuAdy.dato);
+        cuAdy.visitado=true;
+        if(cuartosPorVisitar().size() == 0){
+            System.out.println(cuartosPorVisitar().size());
+            if(cuartosPorVisitar(cuAdy).size()==0){
+                System.out.println(cuartosPorVisitar(cuAdy).size());
+                System.out.println(cadena);
+            }
+        }else{
+            
+        }
+        
     }
 
-    
+    private ArrayList<Cuarto> cuartosPorVisitar() {
+        ArrayList<Cuarto> res = new ArrayList<>();
+        for (int i = 0; i < plano.listaCuarto.size(); i++) {
+            if(plano.listaCuarto.get(i).visitado == false)
+                res.add(plano.listaCuarto.get(i));
+            //System.out.println(plano.listaCuarto.get(i).visitado + plano.listaCuarto.get(i).dato);
+        }
+        return res;
+    }
+
+    private ArrayList<Cuarto> cuartosPorVisitar(Cuarto cuAdy) {
+        ArrayList<Cuarto> res = new ArrayList<>();
+        for (int i = 0; i < cuAdy.cuarto.size(); i++) {
+            if(cuAdy.cuarto.get(i).visitado==false)
+                res.add(cuAdy.cuarto.get(i));
+            //System.out.println(plano.listaCuarto.get(i).visitado);
+        }
+        return res;
+    }
+
+}
